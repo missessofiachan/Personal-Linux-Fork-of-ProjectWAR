8c5223efaa225bb1c01a4cff04a7b4f1a7774a6a
Shmerrick <33600225+Shmerrick@users.noreply.github.com>
2025-08-24 14:29:00 -0400

    Add frame size validation to ApiPacket

--- WorldServer/API/ApiPacket.cs
+++ WorldServer/API/ApiPacket.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using FrameWork;
 
 namespace WorldServer.API
 {
     public class ApiPacket : Packet
     {
-
         private ushort _sequenceID;
 
         public ushort SequenceID
         {
             get { return _sequenceID; }
         }
+
         public ushort _sessionID;
         public ushort _unk1;
         public byte _unk2;
-
 
         public Opcodes OP;
 
@@ -27,27 +23,39 @@ namespace WorldServer.API
             _size = size;
             WriteUInt32(0); //length
             WriteByte((byte)op);
-
         }
 
-     
         public void LoadFrame(int size, CircularBuffer buf)
         {
-            //TODO: Check buffer size
             _size = size;
             _offset = 0;
 
+            if (size + 1 > _data.Length || size + 1 > buf.Data.Length)
+            {
+                Log.Error("ApiPacket", $"Frame size {size} exceeds buffer length");
+                return;
+            }
 
-            buf.Peek(_data, size + 1);
+            if (!buf.Peek(_data, size + 1))
+            {
+                Log.Error("ApiPacket", $"Failed to peek {size + 1} bytes from buffer");
+                return;
+            }
 
             OP = (Opcodes)ReadByte();
         }
 
         public void LoadFrame(int size)
         {
-            //TODO: Check buffer size
             _size = size;
             _offset = 0;
+
+            if (size + 5 > _data.Length)
+            {
+                Log.Error("ApiPacket", $"Frame size {size} exceeds internal buffer length");
+                return;
+            }
+
             ReadUInt32();
             OP = (Opcodes)ReadByte();
         }
@@ -59,13 +67,10 @@ namespace WorldServer.API
             WriteUInt32((ushort)(offset - 5));
             Offset = offset;
         }
+
         public override string ToString()
         {
             return "ControlPacket OP=" + OP.ToString() + " Size=" + _size;
         }
-
-
-
-
     }
 }
